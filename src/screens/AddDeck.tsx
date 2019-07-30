7833f741b9e41a40be8bf15643fbdfe98fe93eff
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-30 16:19:34 +0200

    feat: main screens created

---
+++ src/screens/AddDeck.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AddDeck = () => {
+  return (
+    <View>
+      <Text>Add Deck</Text>
+    </View>
+  );
+};
+
+export default AddDeck;
