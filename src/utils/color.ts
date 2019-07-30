e9b8ffc6038de74caaff25218532d04b1ed80320
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-07-30 16:23:24 +0200

    feat: colors defined

---
+++ src/utils/color.ts
@@ -0,0 +1,10 @@
+export enum Colors {
+  primaryColor = "darkviolet",
+  secondaryColor = "darkkhaki",
+  thirdColor = "navajowhite",
+  successColor = "green",
+  errorColor = "red",
+  warningColor = "yellow",
+  dirtyWhite = "dirtywhite",
+  white = "white"
+}
