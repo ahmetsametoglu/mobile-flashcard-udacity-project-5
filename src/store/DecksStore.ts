6ff31f6f38d9a5930c3a7844b8795dd5a646655a
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-05 11:44:48 +0200

    feat: deck-store created

---
+++ src/store/DecksStore.ts
@@ -0,0 +1,54 @@
+import { DeckService } from "./../utils/api";
+import { IDeck } from "./../models/deck.model";
+import { ActionType } from "./ActionType";
+import { Reducer, Dispatch } from "react";
+
+//#region models
+
+export interface IDecksState {
+  deckList: IDeck[];
+}
+
+interface IDeckPayload {
+  deckList?: IDeck[];
+}
+
+interface IDecksAction {
+  type: ActionType;
+  payload: IDeckPayload | null;
+}
+
+//#endregion models
+
+export class DecksAction {
+  public static fetchDeckList = async (): Promise<IDecksAction> => {
+    const deckList = await DeckService.getDeckList();
+
+    console.log("[Decks Action]: fetchDeckList");
+
+    return {
+      type: ActionType.FetchDeckList,
+      payload: { deckList: deckList }
+    };
+  };
+}
+
+export const initialDecksState: IDecksState = { deckList: [] };
+
+export const DecksReducer: Reducer<IDecksState, IDecksAction> = (
+  state = initialDecksState,
+  action
+) => {
+  console.log(`[Decks Reducer]: ${ActionType[action.type]}`);
+
+  switch (action.type) {
+    case ActionType.FetchDeckList:
+      const deckList = action.payload.deckList;
+      if (!!deckList) {
+        state.deckList = deckList;
+      }
+      return { ...state };
+    default:
+      throw new Error();
+  }
+};
