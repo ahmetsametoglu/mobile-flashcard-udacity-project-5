fdfaa6d8347138d8fb23a9077aaa295f2b6fec09
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-07 13:48:39 +0200

    feat: DeckMenu added to stack-nav

--- src/navigators/StackNav.tsx
+++ src/navigators/StackNav.tsx
@@ -1,14 +1,34 @@
 import { createAppContainer, createStackNavigator } from "react-navigation";
 import TabNav from "./TabNav";
-import Decks from "../screens/Decks";
+import DeckMenu from "../screens/DeckMenu";
+import { Colors } from "../utils/color";
 
-const StackNav = createStackNavigator({
-  home: {
-    screen: TabNav,
-    navigationOptions: {
-      header: null
+const StackNav = createStackNavigator(
+  {
+    Home: {
+      screen: TabNav,
+      navigationOptions: {
+        header: null
+      }
+    },
+    DeckMenu: {
+      screen: DeckMenu,
+      navigationOptions: ({ navigation }) => ({
+        title: `${navigation.state.params.title}`
+      })
+    }
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: Colors.primaryColor,
+      headerStyle: {
+        height: 40,
+        backgroundColor: Colors.white,
+        borderBottomColor: Colors.secondaryColor
+      },
+      headerBackTitle: null
     }
   }
-});
+);
 
 export default createAppContainer(StackNav);
