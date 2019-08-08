0216fe65707091bc83a5479622916b658372bb1b
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-09 01:32:14 +0200

    feat: loading component added to add-deck

--- src/screens/AddDeck.tsx
+++ src/screens/AddDeck.tsx
@@ -8,48 +8,66 @@ import {
   KeyboardAvoidingView,
   TouchableHighlight,
   ScrollView,
-  View
+  View,
+  ActivityIndicator
 } from "react-native";
 import { Colors } from "../utils/color";
 import { INavigationProp } from "../models/props.model";
 import { useStateValue } from "../contexts/StateContext";
+import Loading from "../components/Loading";
 
 interface IProps extends INavigationProp {}
 const AddDeck: FC<IProps> = props => {
   const [deckTitle, setDeckTitle] = useState("");
+  const [showLoading, setShowLoading] = useState(false);
   const { deckAction } = useStateValue();
 
   const onSubmitPage = () => {
     console.log("onSubmitPage");
 
-    deckAction.addDeck(deckTitle).then(_ => props.navigation.navigate("Decks"));
+    setShowLoading(true);
+    deckAction
+      .addDeck(deckTitle)
+      .then(_ => {
+        setShowLoading(false);
+        props.navigation.navigate("Decks");
+      })
+      .catch(_ => {
+        setShowLoading(false);
+      });
   };
 
-  return (
-    <KeyboardAvoidingView
-      style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
-      behavior="padding"
-      keyboardVerticalOffset={150}
-    >
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.title}>What is the title of your new deck ?</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Deck Title"
-            placeholderTextColor={Colors.grey}
-            onSubmitEditing={onSubmitPage}
-            onChangeText={value => {
-              setDeckTitle(value);
-            }}
-          />
-          <TouchableHighlight style={styles.button} onPress={onSubmitPage}>
-            <Text style={{ color: Colors.white, fontSize: 20 }}>Submit</Text>
-          </TouchableHighlight>
-        </View>
-      </ScrollView>
-    </KeyboardAvoidingView>
-  );
+  if (showLoading) {
+    return <Loading text="saving..." />;
+  } else {
+    return (
+      <KeyboardAvoidingView
+        style={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
+        behavior="padding"
+        keyboardVerticalOffset={150}
+      >
+        <ScrollView>
+          <View style={styles.container}>
+            <Text style={styles.title}>
+              What is the title of your new deck ?
+            </Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Deck Title"
+              placeholderTextColor={Colors.grey}
+              onSubmitEditing={onSubmitPage}
+              onChangeText={value => {
+                setDeckTitle(value);
+              }}
+            />
+            <TouchableHighlight style={styles.button} onPress={onSubmitPage}>
+              <Text style={{ color: Colors.white, fontSize: 20 }}>Submit</Text>
+            </TouchableHighlight>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
+  }
 };
 
 const styles = StyleSheet.create<{
