72ef3271f8d6e038cb2aca5b99abe0f0d4ef9d39
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-09 03:19:23 +0200

    feat: AppContext created and AppStore implemented to AppContext

---
+++ src/contexts/AppContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useReducer } from "react";
+import {
+  AppAction,
+  IAppState,
+  appReducer,
+  initialAppState
+} from "../store/AppStore";
+
+interface IAppContext {
+  state: IAppState;
+  appAction: AppAction;
+}
+
+export const AppContext = createContext<IAppContext>(null);
+export const useAppValue = () => useContext(AppContext);
+
+const AppContextProvider = props => {
+  console.log("[AppContext]: init");
+
+  const [appState, dispatch] = useReducer(appReducer, initialAppState);
+  let appAction = new AppAction(dispatch);
+
+  return (
+    <AppContext.Provider value={{ state: appState, appAction }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
