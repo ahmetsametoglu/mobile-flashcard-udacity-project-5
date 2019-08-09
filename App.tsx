6f23cf080fee78fea7a73428a3019c8acb1503c4
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-09 03:21:06 +0200

    feat: AppContextProvider and Loading(hoc) implemented to App.tsx

--- App.tsx
+++ App.tsx
@@ -5,28 +5,34 @@ import StackNav from "./src/navigators/StackNav";
 import Constants from "expo-constants";
 import { Colors } from "./src/utils/color";
 import StateContextProvider from "./src/contexts/StateContext";
+import Loading from "./src/hoc/Loading";
+import AppContextProvider, { AppContext } from "./src/contexts/AppContext";
 
 const App = () => {
   console.log("[App]: init");
 
   return (
-    <StateContextProvider>
-      <View style={{ flex: 1 }}>
-        <View
-          style={{
-            backgroundColor: Colors.thirdColor,
-            height: Constants.statusBarHeight
-          }}
-        >
-          <StatusBar
-            translucent
-            backgroundColor={Colors.thirdColor}
-            barStyle="light-content"
-          />
+    <AppContextProvider>
+      <StateContextProvider>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              backgroundColor: Colors.thirdColor,
+              height: Constants.statusBarHeight
+            }}
+          >
+            <StatusBar
+              translucent
+              backgroundColor={Colors.thirdColor}
+              barStyle="light-content"
+            />
+          </View>
+          <Loading>
+            <StackNav />
+          </Loading>
         </View>
-        <StackNav />
-      </View>
-    </StateContextProvider>
+      </StateContextProvider>
+    </AppContextProvider>
   );
 };
 
