b9abdf2217490e579d96c0e66a0bf0fd59252223
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-09 03:18:12 +0200

    feat: loading component converted to HOC

---
+++ src/hoc/Loading.tsx
@@ -0,0 +1,41 @@
+import React, { FC, Fragment } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import { Colors } from "../utils/color";
+import { useAppValue } from "../contexts/AppContext";
+
+interface IProp {}
+
+const Loading: FC<IProp> = props => {
+  const { state } = useAppValue();
+
+  if (!state.showLoading) {
+    return <Fragment>{props.children}</Fragment>;
+  } else {
+    return (
+      <Fragment>
+        {props.children}
+        <View
+          style={{
+            zIndex: 50,
+            position: "absolute",
+            flex: 1,
+            height: "100%",
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: Colors.loadingBackRoundColor
+          }}
+        >
+          <ActivityIndicator size={50} color={Colors.primaryColor} />
+          {!!state.loadingText && (
+            <Text style={{ fontSize: 20, color: Colors.red, marginTop: 10 }}>
+              {state.loadingText}
+            </Text>
+          )}
+        </View>
+      </Fragment>
+    );
+  }
+};
+
+export default Loading;
