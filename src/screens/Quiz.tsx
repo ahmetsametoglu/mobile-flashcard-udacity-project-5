1ae2b6442fbbf2f848fb1033fde06ca0e8f7220b
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-10 14:21:35 +0200

    feat: quiz screen designed

--- src/screens/Quiz.tsx
+++ src/screens/Quiz.tsx
@@ -1,14 +1,88 @@
-import React, { FC } from "react";
-import { View, Text } from "react-native";
+import React, { FC, useState, Fragment } from "react";
+import { View, Text, StyleSheet, ViewStyle, TextStyle } from "react-native";
 import { INavigationProp } from "../models/props.model";
+import { useStateValue } from "../contexts/StateContext";
+import QuestionCard from "../components/QuestionCard/QuestionCard";
+import { Colors } from "../utils/color";
 
 interface IProps extends INavigationProp {}
-const Quiz: FC<IProps> = () => {
+
+const Quiz: FC<IProps> = props => {
+  const { navigation } = props;
+  const { state } = useStateValue();
+
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(1);
+
+  const deckId = navigation.state.params.id;
+  const selectedDeck = state.deckList.find(d => d._id === deckId);
+
+  if (!selectedDeck || selectedDeck.cards.length === 0) {
+    return <ThereIsNoCard />;
+  }
+
+  const handleAnswerQuestion = () => {
+    setScore(score + 1);
+    setCurrentQuestion(currentQuestion + 1);
+  };
+
+  const scoreView = (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={styles.scoreInfo}>{`score: ${(score /
+        selectedDeck.cards.length) *
+        100}`}</Text>
+    </View>
+  );
+  const questionView = (
+    <Fragment>
+      <View style={styles.infoSection}>
+        <Text style={styles.quizInfo}>{`${currentQuestion}/${
+          selectedDeck.cards.length
+        }`}</Text>
+        <Text style={styles.scoreInfo}>{`score: ${score}`}</Text>
+      </View>
+      <QuestionCard
+        question={selectedDeck.cards[currentQuestion - 1]}
+        onAnswerQuestion={handleAnswerQuestion}
+      />
+    </Fragment>
+  );
+
   return (
-    <View>
-      <Text>Quiz</Text>
+    <View style={{ flex: 1, padding: 20 }}>
+      {currentQuestion > selectedDeck.cards.length ? scoreView : questionView}
     </View>
   );
 };
 
+const styles = StyleSheet.create<{
+  infoSection: ViewStyle;
+  quizInfo: TextStyle;
+  scoreInfo: TextStyle;
+}>({
+  infoSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 10
+  },
+  quizInfo: {
+    fontSize: 20,
+    color: Colors.errorColor
+  },
+  scoreInfo: {
+    fontSize: 20,
+    color: Colors.successColor
+  }
+});
+
 export default Quiz;
+
+const ThereIsNoCard: FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ margin: 25, fontSize: 23, textAlign: "center" }}>
+        Sorry, you can't take a quiz because there are no card in the deck.
+      </Text>
+    </View>
+  );
+};
