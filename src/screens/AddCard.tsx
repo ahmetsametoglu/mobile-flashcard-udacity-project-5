d646823c8c69f6f8a04cba0fe414657a64d6b98d
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-12 20:56:46 +0200

    feat: AddCard form created

--- src/screens/AddCard.tsx
+++ src/screens/AddCard.tsx
@@ -1,15 +1,104 @@
-import React, { FC } from "react";
-import { View, Text } from "react-native";
+import React, { FC, useState } from "react";
+import { View, StyleSheet, TextStyle, ViewStyle } from "react-native";
 import { INavigationProp } from "../models/props.model";
+import KeyboardView from "../hoc/KeyboardView";
+import MyInput from "../components/UI/MyInput";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { Text } from "react-native-elements";
+import { Colors } from "../utils/color";
+import { useStateValue } from "../contexts/StateContext";
+import { useAppValue } from "../contexts/AppContext";
 
 interface IProps extends INavigationProp {}
 
-const AddCard: FC<IProps> = () => {
+const AddCard: FC<IProps> = props => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const { deckAction } = useStateValue();
+  const { appAction } = useAppValue();
+
+  const { deckId } = props.navigation.state.params;
+
+  const onSubmitPage = () => {
+    console.log("onSubmitPage");
+
+    if (question !== "" && answer !== "") {
+      appAction.showLoading("saving...");
+      deckAction
+        .addCard(deckId, question, answer)
+        .then(_ => {
+          appAction.hideLoading();
+          setQuestion("");
+          setAnswer("");
+          props.navigation.pop();
+        })
+        .catch(_ => {
+          appAction.hideLoading();
+        });
+    }
+  };
+
   return (
-    <View>
-      <Text>Add Card</Text>
-    </View>
+    <KeyboardView>
+      <View style={styles.container}>
+        <MyInput
+          labelText="question"
+          placeHolder="question"
+          onValueChanged={value => setQuestion(value)}
+          value={question}
+        />
+        <MyInput
+          labelText="answer"
+          placeHolder="answer"
+          onValueChanged={value => setAnswer(value)}
+          value={answer}
+        />
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={onSubmitPage}
+          underlayColor={Colors.thirdColor}
+        >
+          <Text style={{ color: Colors.white, fontSize: 20 }}>Submit</Text>
+        </TouchableHighlight>
+      </View>
+    </KeyboardView>
   );
 };
+
+const styles = StyleSheet.create<{
+  container: ViewStyle;
+  input: TextStyle;
+  button: ViewStyle;
+}>({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20
+  },
+  input: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 18,
+    marginTop: 40,
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row",
+    borderRadius: 5,
+    width: "90%"
+  },
+  button: {
+    marginTop: 50,
+    backgroundColor: Colors.primaryColor,
+    height: 50,
+    width: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5
+  }
+});
 
 export default AddCard;
