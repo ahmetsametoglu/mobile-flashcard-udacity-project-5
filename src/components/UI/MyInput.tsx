967d22e95628bb9ea40ac9384dc821edbde8840b
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-12 20:55:08 +0200

    feat: my-input component created

---
+++ src/components/UI/MyInput.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input } from "react-native-elements";
+import { Colors } from "../../utils/color";
+
+interface IProps {
+  placeHolder: string;
+  onValueChanged: Function;
+  value: string;
+  labelText: string;
+  iconName?: string;
+}
+const MyInput: FC<IProps> = props => {
+  return (
+    <Input
+      value={props.value}
+      onChangeText={value => {
+        props.onValueChanged(value);
+      }}
+      label={props.labelText}
+      placeholder={props.placeHolder}
+      errorStyle={{ color: "red" }}
+      leftIconContainerStyle={{ margin: 10 }}
+      leftIcon={
+        !!props.iconName ? (
+          <Icon name={props.iconName} size={24} color={Colors.primaryColor} />
+        ) : null
+      }
+    />
+  );
+};
+
+export default MyInput;
