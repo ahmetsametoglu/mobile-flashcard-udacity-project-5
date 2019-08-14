d36d6219a63b5e0ef36c47fe995e4e7cd95dfcac
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-14 15:30:08 +0200

    feat: added animation when clicked to card

--- src/components/deck-item.tsx
+++ src/components/deck-item.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Text,
   StyleSheet,
   ViewStyle,
   TextStyle,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated
 } from "react-native";
 import { IDeck } from "../models/deck.model";
 import { Colors } from "../utils/color";
 import { INavigationProp } from "../models/props.model";
-import { NavigationPages } from "../navigators/navigation-pages";
 import { getCardCountText } from "../utils/helper";
+import { NavigationPages } from "../navigators/navigation-pages";
 
 type Props = {
   deck: IDeck;
@@ -19,21 +20,34 @@ type Props = {
 const DeckItem = (props: Props) => {
   const { deck, navigation } = props;
 
+  const scale = useRef(new Animated.Value(150)).current;
+
+  const navigateToCardMenu = () => {
+    Animated.sequence([
+      Animated.timing(scale, { toValue: 100, duration: 200 }),
+      Animated.timing(scale, { toValue: 150, duration: 300 })
+    ]).start(() => {
+      navigation.push(NavigationPages.DeckMenu, {
+        deckId: deck._id,
+        title: deck.title
+      });
+    });
+  };
+
   return (
-    <TouchableOpacity
-      style={styles.card}
-      onPress={() => {
-        navigation.push(NavigationPages.DeckMenu, {
-          deckId: deck._id,
-          title: deck.title
-        });
-      }}
-    >
-      <Text style={styles.title}>{deck.title}</Text>
-      <Text style={styles.cardNumber}>
-        {getCardCountText(deck.cards.length)}
-      </Text>
-    </TouchableOpacity>
+    <Animated.View style={[styles.card, { height: scale }]}>
+      <TouchableOpacity
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        onPress={() => {
+          navigateToCardMenu();
+        }}
+      >
+        <Text style={styles.title}>{deck.title}</Text>
+        <Text style={styles.cardNumber}>
+          {getCardCountText(deck.cards.length)}
+        </Text>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
@@ -43,10 +57,15 @@ const styles = StyleSheet.create<{
   cardNumber: TextStyle;
 }>({
   card: {
-    borderBottomWidth: 2,
-    borderColor: Colors.thirdColor,
-    height: 150,
-    justifyContent: "center"
+    justifyContent: "center",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    backgroundColor: Colors.loadingBackRoundColor,
+    borderBottomWidth: 0,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+    elevation: 2
   },
   title: {
     color: Colors.primaryColor,
