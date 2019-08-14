170d83b9d68e0c55f1553f242bc59b1f169d2937
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-14 10:19:09 +0200

    feat: new card added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,6 +6,45 @@ import { IDeck } from "./../models/deck.model";
 
 let _deckList: IDeck[] = [
   {
+    _id: "0",
+    title: "how well do you know the world",
+    cards: [
+      {
+        _id: "01",
+        question: "How many stars are there on the flag of China?",
+        answer: "5"
+      },
+      {
+        _id: "02",
+        question: "What is the currency of Mongolia?",
+        answer: "Tugrik"
+      },
+      {
+        _id: "03",
+        question:
+          "In which country is there a natural gas pit nicknamed the ‘Door to Hell’ that has been on fire since 1971?",
+        answer: "Turkmenistan"
+      },
+      {
+        _id: "04",
+        question:
+          "In 2013 which two airlines merged to become the world’s largest airline?",
+        answer: "American Airlines and US Airways"
+      },
+      {
+        _id: "05",
+        question:
+          "Which country has more lakes than the rest of the world combined?",
+        answer: "Canada"
+      },
+      {
+        _id: "06",
+        question: "Which country has the world's highest waterfall ?",
+        answer: "Venezuela"
+      }
+    ]
+  },
+  {
     _id: "1",
     title: "React",
     cards: [
@@ -41,11 +80,12 @@ export class DeckService {
       setTimeout(async () => {
         try {
           const value = await AsyncStorage.getItem("decks");
+
           if (value !== null) {
             const data = JSON.parse(value) as IDeck[];
             _deckList = data.map(x => Object.assign({}, x));
-            res([...data]);
           }
+          res([..._deckList]);
         } catch (error) {
           rej(error);
         }
