0b86d9bdfd5596e1934b7d81ce5c1a3bb19fd081
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-08-14 12:53:05 +0200

    refactor: color enum changed to const

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,14 +1,14 @@
-export enum Colors {
-  primaryColor = "darkviolet",
-  secondaryColor = "darkgoldenrod",
-  thirdColor = "navajowhite",
-  successColor = "green",
-  errorColor = "red",
-  warningColor = "yellow",
-  grey = "grey",
-  white = "white",
-  darkgoldenrod = "darkgoldenrod",
-  black = "black",
-  red = "red",
-  loadingBackRoundColor = "rgba(246, 246, 235, 0.8)"
-}
+export const Colors = {
+  primaryColor: "darkviolet",
+  secondaryColor: "darkgoldenrod",
+  thirdColor: "navajowhite",
+  successColor: "green",
+  errorColor: "red",
+  warningColor: "yellow",
+  grey: "grey",
+  white: "white",
+  darkgoldenrod: "darkgoldenrod",
+  black: "black",
+  red: "red",
+  loadingBackRoundColor: "rgba(246, 246, 235, 0.8)"
+};
